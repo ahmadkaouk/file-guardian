c4a32402d0fce3736f04709a9fd75d539eb887f7
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-05-30 21:41:39 +0200

    Add the Hasher trait

---
+++ merkle-tree/src/hasher.rs
@@ -0,0 +1,39 @@
+///  # Hasher Trait
+/// 
+///  This trait defines the interface for a hash function that can be used with a
+///  Merkle tree. By implementing this trait, you can use any hash function with
+///  the Merkle tree, making it more flexible and customizable.
+/// 
+///  ## Usage
+/// 
+///  To use this trait, you need to implement the `Hasher` trait for your hash
+///  function. The trait has a single method, `hash`, which takes a byte slice
+///  and returns a fixed-size array of bytes:
+/// 
+///  ```rust
+///  use sha2::{Digest, Sha256};
+///  use merkle_tree::Hasher;
+/// 
+///  struct Sha256Hasher;
+/// 
+///  impl Hasher for Sha256Hasher {
+///      type Hash = [u8; 32];
+/// 
+///      fn hash<T: AsRef<[u8]>>(&self, data: T) -> Self::Hash {
+///          let mut hasher = Sha256::new();
+///          hasher.update(data);
+///          hasher.finalize().into()
+///      }
+///  }
+///  let hash = MyHasher::hash("hello world".as_bytes());
+///  assert_eq!(hash, [185, 77, 39, 185, 147, 77, 62, 8, 165, 46, 82, 215, 218,
+///  125, 171, 250, 196, 132, 239, 227, 122, 83, 128, 238, 144, 136, 247, 172,
+///  226, 239, 205, 233]); 
+/// ```
+pub trait Hasher {
+    /// The output type of the hash function.
+    type Hash: Clone + AsRef<[u8]> + Default + PartialEq;
+
+    /// Computes the hash of the given data.
+    fn hash<T: AsRef<[u8]>>(&self, data: T) -> Self::Hash;
+}
---
+++ merkle-tree/src/lib.rs
@@ -0,0 +1,34 @@
+//! # Merkle Tree
+//!
+//! This package provides a Merkle tree implementation in Rust. A Merkle tree is
+//! a hash-based data structure that is used to verify the integrity of data. It
+//! is commonly used in distributed  systems, such as blockchain, to ensure that
+//! data has not been tampered with.
+//!
+//! ## Usage
+//!
+//! To use this package, add the following to your `Cargo.toml` file:
+//!
+//! ```toml
+//! [dependencies]
+//! merkle-tree = "0.1.0"
+//! ```
+//!
+//! Then, in your Rust code, you can use the `MerkleTree` struct to create a new
+//! Merkle tree:
+//!
+//! ```rust
+//! use merkle_tree::MerkleTree;
+//!
+//! let data = vec!["hello", "world"];
+//! let tree = MerkleTree::new(data);
+//! ```
+//!
+//!
+//! To see examples of how to use this package, please refer to the `examples`
+//! directory in the source code.
+//!
+//! ## References
+//!
+//! * [Merkle tree - Wikipedia](https://en.wikipedia.org/wiki/Merkle_tree)
+//! * [Mastering Bitcoin: Unlocking Digital Cryptocurrencies](https://www.oreilly.com/library/view/mastering-bitcoin/9781491902639/ch07.html)

8773c998e569c83a30bd7172db681757cac65bcf
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-05-31 16:55:45 +0200

    Add basic cli support for client

---
+++ client/src/cli.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, about, version)]
+pub struct Args {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Subcommand)]
+enum SubCommand {
+    /// List all the uploaded files
+    List,
+    /// Upload one or more files(s) to the server
+    Upload {
+        #[arg(short, long, value_name = "FILES")]
+        #[clap(required = true)]
+        files: Vec<PathBuf>,
+    },
+    /// Download a file from the server
+    Download {
+        #[arg(short, long, value_name = "FILE")]
+        #[clap(required = true)]
+        file: String,
+    },
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,8 @@
+use clap::Parser;
+use cli::Args;
+mod cli;
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
